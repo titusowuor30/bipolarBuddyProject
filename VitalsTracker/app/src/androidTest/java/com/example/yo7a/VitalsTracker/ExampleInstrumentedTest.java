fabe72d17db2bd039e8efbd28b76b82db222f64e
Titus Owuor <54272773+titusowuor30@users.noreply.github.com>
2024-07-16 16:35:25 +0300

    apk

--- VitalsTracker/app/src/androidTest/java/com/example/yo7a/VitalsTracker/ExampleInstrumentedTest.java
+++ VitalsTracker/app/src/androidTest/java/com/example/yo7a/VitalsTracker/ExampleInstrumentedTest.java
@@ -10,17 +10,16 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumentation test, which will execute on an Android device.
+ * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() throws Exception {
+    public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.yo7a.VitalsTracker", appContext.getPackageName());
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.bengohub.VitalsTracker", appContext.getPackageName());
     }
 }
